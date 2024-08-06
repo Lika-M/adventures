456e4f1a544b22c8b7537815649efb405c10b015
Iglika Mincheva <103664008+Lika-M@users.noreply.github.com>
2024-08-06 11:29:14 +0300

    Added a file-based router component 'Adventures' which retrieves data from the database and renders the adventures list.

--- app/adventures/page.tsx
+++ app/adventures/page.tsx
@@ -1,5 +1,17 @@
-export default function Home() {
-    return (
-      <p>Adventures Page</p>
-    );
+import { getAllAdventures } from "@/lib/mongodb";
+import AdventureList from "@/components/adventures/adventure-list";
+
+import { type Adventure } from "@/types";
+
+export default async function Adventures() {
+
+  let adventures: Adventure[] = [];
+
+  try {
+    adventures = await getAllAdventures();
+  } catch (error) {
+    throw new Error('An error occurred. Please try again later.')
   }
+
+  return <AdventureList adventures={adventures} />
+}
